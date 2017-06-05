2a78bcf70535936d8d5ee0f91a5d8c239c0c7b6e
Sahil Sehgal <sahilsehgal1995@gmail.com>
2017-06-05 09:11:55 +0530

    projects module created

--- src/app/module/projects/projects.module.ts
+++ src/app/module/projects/projects.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const router: Routes = [
+    { path: 'projects', component: ProjectsComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    routes
   ],
   declarations: [ProjectsComponent]
 })
